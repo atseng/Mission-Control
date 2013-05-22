1f74e14f1b56fd3a883045ef00d9a2de24620f44
Allen <atseng247@gmail.com>
2013-05-22 18:35:51 -0400

    MC in/out complete

--- lib/mission_control_command.rb
+++ lib/mission_control_command.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 class MissionControlCommand
   def initialize(cmd)
     @cmd = cmd
@@ -7,12 +9,6 @@ class MissionControlCommand
     ENV['HOME'] + '/check_in'
   end
 
-  def time_spent 
-  end
-
-  def delete_file
-  end
-
   def output
     time = Time.now.strftime '%l:%M %p'
     if @cmd == 'in'
@@ -20,17 +16,21 @@ class MissionControlCommand
         'Already signed in'
       else
         File.open(self.class.check_in_path, 'w') { |f| f.puts time }
-        'You are checked in as of ' + time
+        'You are checked in as of' + time
       end
     elsif @cmd == 'out'
       if FileTest.exists?(self.class.check_in_path) == false
         'Already signed out'
       else
+        file = File.open(ENV['HOME'] + '/check_in', 'rb')
+        check_in_time = file.read
+        time_spent = Time.now - Time.parse(check_in_time)
+        time_spent = (time_spent.to_i / 60).to_s
         File.delete(ENV['HOME'] + '/check_in')
-        puts 'You are checked out as of ' + time
-        puts 'Total time spent ' + time_spent
+        'You are checked out as of' + time + "\n" + 'Total time spent: ' + time_spent + ' minutes'
       end
-
+    else
+      'Not a valid input'
     end
   end
 end
--- spec/mission_control_command_spec.rb
+++ spec/mission_control_command_spec.rb
@@ -1,14 +1,19 @@
 require 'rspec'
-require 'time'
 
 require_relative '../lib/mission_control_command'
 
 describe MissionControlCommand do
 
-  context 'checking in' do
+  context 'no checkin file' do
+    it 'cannot sign out if already signed out' do
+      cmd = MissionControlCommand.new('out')
+      cmd2 = MissionControlCommand.new('out')
+      expect(cmd2.output).to include('Already signed out')
+    end
+
     it 'acknowledges my check in' do
       cmd = MissionControlCommand.new('in')
-      expect(cmd.output).to include('You are checked in')
+      expect(cmd.output).to include('checked in')
     end
 
     it 'creates a check in file' do
@@ -19,12 +24,6 @@ describe MissionControlCommand do
       end
       cmd.output
       expect(check_in_path).to be_true
-    end
-
-    it 'cannot sign in if already checked in' do
-      cmd = MissionControlCommand.new('in')
-      cmd2 = MissionControlCommand.new('in')
-      expect(cmd2.output).to include('Already signed in')
     end
 
     it 'shows time of sign in' do
@@ -38,10 +37,16 @@ describe MissionControlCommand do
     end
   end
 
-  context 'checking out' do
+  context 'if checkin file exists' do
+    it 'cannot sign in if already checked in' do
+      cmd = MissionControlCommand.new('in')
+      cmd2 = MissionControlCommand.new('in')
+      expect(cmd2.output).to include('Already signed in')
+    end
+
     it 'acknowledges my check out' do
       cmd = MissionControlCommand.new('out')
-      expect(cmd.output).to include('You are checked out')
+      expect(cmd.output).to include('checked out')
     end
 
     it 'removes the file on check out' do
@@ -50,28 +55,13 @@ describe MissionControlCommand do
       expect(ENV['HOME'].include?(check_in_path)).to be_false
     end
 
-    it 'cannot sign out if already signed out' do
-      cmd = MissionControlCommand.new('out')
-      cmd2 = MissionControlCommand.new('out')
-      expect(cmd2.output).to include('Already signed out')
-    end
-
-    it "shouldn't show a time at sign out" do
+    it 'shows time of sign out and time spent' do
       cmd = MissionControlCommand.new('out')
       check_in_path = cmd.class.check_in_path
       if FileTest.exists?(check_in_path)
         time = Time.now.strftime '%l:%M %p'
-        expect(cmd.output).to_not include(time)
+        expect(cmd.output).to include(time, 'Total time spent')
       end
-
-    # it 'shows duration of time spent on sign out'
-    #   cmd = MissionControlCommand.new('out')
-    #   check_in_path = cmd.class.check_in_path
-    #   if FileTest.exists?(check_in_path)
-    #     time_spent = Time.now -time_start
-    #     expect(cmd.output).to include('Total time spent:')
-    #     expect(cmd.output).to include(time_spent)
-    # end
     end
   end
 end
